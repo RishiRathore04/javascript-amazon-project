566376947c9f3ea72b01119b58d96be3e9bec6cb
rishi rahore <rathorerishi299@gmail.com>
2025-09-20 00:44:53 +0530

    save data and generate HTML for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,8 +1,9 @@
 import {cart, removeFromCart, updateDeliveryOptions} from '../../data/cart.js';
-import {products} from '../../data/products.js';
+import {products, getProduct } from '../../data/products.js';
 import  formatCurrency  from '../utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import { deliveryOptions } from '../../data/deliveryOptions.js';
+import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 console.log(dayjs());
 console.log(dayjs().add(7,'day'));
@@ -14,22 +15,11 @@ export function renderOrderSummary() {
   cart.forEach((cartItem) => {
     const productId = cartItem.productId;
 
-    let matchingProduct;
-    products.forEach((product) => {  // this is a example of deduplicating or normalizing the data
-      if(product.id == productId){    
-        matchingProduct = product;
-      }		
-    });
+    const matchingProduct = getProduct(productId);
     
     const deliveryOptionId = cartItem.deliveryOptionId;
 
-    let deliveryOption;
-
-    deliveryOptions.forEach((option) => {
-      if(option.id === deliveryOptionId) {
-        deliveryOption = option;
-      }
-    });
+    const deliveryOption = getDeliveryOption(deliveryOptionId);
 
     const today = dayjs();
       const deliveryDate = today.add(
@@ -149,6 +139,7 @@ export function renderOrderSummary() {
           const {productId, deliveryOptionId} = element.dataset;
           updateDeliveryOptions(productId, deliveryOptionId);
           renderOrderSummary();
+          renderPaymentSummary();
         });
       });
 }      
