277ac0143a77744fb36076b69868d79a3cd221b7
rishi rahore <rathorerishi299@gmail.com>
2026-01-25 13:52:54 +0530

    convert product objects into class

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
-import '../data/cart-class.js'; //use to run all the codes inside the file without importing anything
+//import '../data/cart-class.js'; //use to run all the codes inside the file without importing anything
 
 renderCheckoutHeader();
 renderOrderSummary();
