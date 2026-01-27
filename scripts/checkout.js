d2d753977bd6dd46148345e1fab9b227327925bf
rishi rahore <rathorerishi299@gmail.com>
2026-01-27 13:57:03 +0530

    practised the class , inheritance , getter and setter by creating car Class

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,7 @@ import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 //import '../data/cart-class.js'; //use to run all the codes inside the file without importing anything
+//import '../data/car.js';
 
 renderCheckoutHeader();
 renderOrderSummary();
